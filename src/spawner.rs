cfc514ae4e0ab3eb99020e582ff3f64fd4dad418
Benedikt Schnatterbeck <benediktms@hey.com>
2022-07-23 13:09:53 +0100

    Add amulet of yala component

--- src/spawner.rs
+++ src/spawner.rs
@@ -47,3 +47,16 @@ pub fn make_goblin() -> (i32, String, FontCharType) {
 pub fn make_orc() -> (i32, String, FontCharType) {
     (2, "Orc".to_string(), to_cp437('o'))
 }
+
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        AmuletOfYala,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('|'),
+        },
+        Name("Amulet of Yala".to_string()),
+    ));
+}
